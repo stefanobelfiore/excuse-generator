a1f528fc595729ca54fccd9e551dbe444dd72fea
stefanobelfiore <stefanobelfiore@yahoo.com>
2021-04-25 07:32:57 +0000

    proyecto acabado

--- src/app.js
+++ src/app.js
@@ -4,8 +4,45 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+const PRONOUN = ["A ", "The ", "My "];
+const SUBJECT = ["dog ", "turtle ", "bird ", "Jogger ", "Driver "];
+const ACTION = ["ate ", "peed ", "crushed ", "broke ", "took "];
+const WHAT = ["my homework ", "the keys ", "the car ", "my shoes "];
+const WHEN = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateExcuse(printExcuse());
+};
+let inventExcuse = () => {
+  let pronounIndex = Math.floor(Math.random() * PRONOUN.length);
+  let subjectIndex = Math.floor(Math.random() * SUBJECT.length);
+  let actionIndex = Math.floor(Math.random() * ACTION.length);
+  let whatIndex = Math.floor(Math.random() * WHAT.length);
+  let whenIndex = Math.floor(Math.random() * WHEN.length);
+  let pronounExcuse = PRONOUN[pronounIndex];
+  let subjectExcuse = SUBJECT[subjectIndex];
+  let actionExcuse = ACTION[actionIndex];
+  let whatExcuse = WHAT[whatIndex];
+  let whenExcuse = WHEN[whenIndex];
+  return pronounExcuse.concat(
+    subjectExcuse,
+    actionExcuse,
+    whatExcuse,
+    whenExcuse
+  );
+};
+let printExcuse = () => {
+  let myExcuse = document.querySelector("#excuse");
+  myExcuse.innerHTML = inventExcuse();
+};
+let generateExcuse = () => {
+  let myButton = document.querySelector("#buttonExcuse");
+  myButton.addEventListener("click", event => {
+    printExcuse();
+  });
 };
